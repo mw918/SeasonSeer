913c0e50496dac67c749580d5d38e779e3e9d9b6
Michael Wang <mw918@hotmail.com>
2020-07-13 00:35:41 -0400

    Added ability to check out players by position

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SeasonSeer
+To operate this, 
+1. Install Python on your computer
+2. Go to the 'server' folder in this repo on the command program of your choice
+3. Type "python apiInterface.py" in the command line and press enter
+4. Go to your browser of choice and type in the address line "localhost:5000"
--- server/apiInterface.py
+++ server/apiInterface.py
@@ -11,18 +11,122 @@ def get_teams():
   data = response.json()
   return data["teams"]
 
-#def get_player_stats():
+def get_player_stats(player):
+    currentPlayer = list()
+    try:
+      currentYear = datetime.datetime.now().year
+      player_base = "https://statsapi.web.nhl.com/api/v1/people/"+str(player["person"]["id"])+"/stats?stats=statsSingleSeason&season="+str(currentYear-1)+str(currentYear)
+      playerResponse = requests.get(player_base)
+      playerData = playerResponse.json()
+      #This variable is the list of all stats for one player
+      playerStats = playerData['stats'][0]['splits'][0]['stat']
+      currentPlayer.append(player["person"]["fullName"])
+      currentPlayer.append(player["jerseyNumber"])
+      currentPlayer.append(player["position"]["abbreviation"])
+      #Goalie stats
+      if (player["position"]["abbreviation"]=='G'):
+        win = request.args.get('win', type = float) * playerStats["wins"]
+        ga = request.args.get('ga', type = float) * playerStats["goalsAgainst"]
+        save = request.args.get('save', type = float) * playerStats["saves"]
+        shutout = request.args.get('so', type = float) * playerStats["shutouts"]
+        fantasyScore = round(win + ga + save + shutout, 1)
+        currentPlayer.append(str(fantasyScore))
+        currentPlayer.append(str(playerStats["wins"]))
+        currentPlayer.append(str(playerStats["goalsAgainst"]))
+        currentPlayer.append(str(playerStats["saves"]))
+        currentPlayer.append(str(playerStats["shutouts"]))
+      #Skater stats
+      else:
+        goals = request.args.get('goals', type = float) * playerStats["goals"]
+        assists = request.args.get('assists', type = float) * playerStats["assists"]
+        pim = request.args.get('pim', type = float) * playerStats["pim"]
+        ppp = request.args.get('ppp', type = float) * playerStats["powerPlayPoints"]
+        shp = request.args.get('shp', type = float) * playerStats["shortHandedPoints"]
+        sog = request.args.get('sog', type = float) * playerStats["shots"]
+        hit = request.args.get('hit', type = float) * playerStats["hits"]
+        blk = request.args.get('blk', type = float) * playerStats["blocked"]
+        fantasyScore = goals + assists + pim + ppp + shp + sog + hit + blk
+        currentPlayer.append(str(fantasyScore))
+        currentPlayer.append(str(playerStats["games"]))
+        currentPlayer.append(str(playerStats["goals"]))
+        currentPlayer.append(str(playerStats["assists"]))
+        currentPlayer.append(str(playerStats["points"]))
+        currentPlayer.append(str(playerStats["pim"]))
+        currentPlayer.append(str(playerStats["powerPlayPoints"]))
+        currentPlayer.append(str(playerStats["shortHandedPoints"]))
+        currentPlayer.append(str(playerStats["shots"]))
+        currentPlayer.append(str(playerStats["hits"]))
+        currentPlayer.append(str(playerStats["blocked"]))
+    #This only happens for players who are on the roster, but haven't played any games
+    except IndexError:
+      print("No stats for "+currentPlayer[0])
+    return currentPlayer
 
+def check_viability(position):
+  #These are the positions that the users selected (LW, RW, C, D, G, F, S)
+  #Position is the current player
+  allowedPositions = request.args.get('positions', type = str)
+  if (allowedPositions=="left"):
+    return (position=='LW')
+  elif (allowedPositions=='right'):
+    return (position=='RW')
+  elif (allowedPositions=='center'):
+    return (position=='C')
+  elif (allowedPositions=='defense'):
+    return (position=='D')
+  elif (allowedPositions=='goalie'):
+    return (position=='G')
+  elif (allowedPositions=='all'):
+    return True
+  elif (allowedPositions=='skater'):
+    return (position!='G')
+  elif (allowedPositions=='forward'):
+    return (position!='G' and position!='D')
+
+def get_all_players():
+  teams = get_teams()
+  all_players = list()
+  #x is an array of teams and their stats
+  for x in teams:
+    api_url_base = 'https://statsapi.web.nhl.com/api/v1/teams/' + str(x["id"]) + '/roster'
+    teamResponse = requests.get(api_url_base)
+    teamData = teamResponse.json()
+    print("Writing "+x["name"])
+    #i is an array of players and their stats
+    for i in teamData["roster"]:
+      try:
+        if check_viability(i["position"]["abbreviation"]):
+          currentPlayer = get_player_stats(i)
+          all_players.append(currentPlayer)
+      except IndexError:
+        print("No stats for "+i["person"]["fullName"])
+  print ("Begin sorting...")
+  all_players.sort(reverse=True, key=lambda x: float(x[3]))
+  return all_players
+
+def return_players_last_season():
+  startTime = time.perf_counter()
+  print("Begin printing...")
+  allPlayers = get_all_players()
+  returnString = "<style>table, th, td {border: 2px solid powderblue;}</style>"
+  returnString = returnString + '<table style="float:center"><caption>Most fantasy points last season</caption>'
+  returnString = returnString + "<tr><th>Name</th><th>Number</th><th>Position</th><th>Fantasy Score</th></tr>"
+  #<th>GP</th><th>Goals</th><th>Assists</th><th>Points</th><th>PIM</th><th>PPP</th><th>SHP</th><th>SOG</th><th>HIT</th><th>BLK</th></tr>
+  #This goes through the entire list of players
+  #i is an array of a player and their stats
+  for i in allPlayers:
+    returnString = returnString +'<td>'+str(i[0]) +'</td>' +'<td>'+str(i[1]) +'</td>' +'<td>'+str(i[2]) +'</td>' +'<td>'+str(i[3]) +'</td></tr>'
+  returnString = returnString+"</table>"
+  endTime = time.perf_counter()
+  print("App ran in "+str(endTime-startTime)+" seconds")
+  return returnString
 
 def return_teams():
     startTime = time.perf_counter()
     teams = get_teams()
-    skaterStats = list()
-    goalieStats = list()
     rosterString = "<style>table, th, td {border: 2px solid powderblue;}</style>"
     goalieString = "<table style='float:center'><caption>Goalie Stats</caption>"
     goalieString = goalieString + "<tr><th>Name</th><th>Number</th><th>Position</th><th>Team</th><th>Fantasy Score</th><th>Wins</th><th>GA</th><th>SV</th><th>SO</th></tr>"
-    currentYear = datetime.datetime.now().year
     #This loop goes through the teams one by one
     for x in teams:
         api_url_base = 'https://statsapi.web.nhl.com/api/v1/teams/' + str(x["id"]) + '/roster'
@@ -36,35 +140,21 @@ def return_teams():
         #This loop goes through the members of the roster one by one
         for i in teamData["roster"]:
           try:
-            player_base = "https://statsapi.web.nhl.com/api/v1/people/"+str(i["person"]["id"])+"/stats?stats=statsSingleSeason&season="+str(currentYear-1)+str(currentYear)
-            playerResponse = requests.get(player_base)
-            playerData = playerResponse.json()
-            #This variable is the list of all stats for one player
-            currentPlayer = list()
-            playerStats = playerData['stats'][0]['splits'][0]['stat']
-            playerSpecs = '<tr><td><a href="https://www.nhl.com/player/'+str(i["person"]["id"])+'"target="_blank">'+i["person"]["fullName"]+'</a></td><td>'+i["jerseyNumber"]+'</td><td>'+i["position"]["abbreviation"]+'</td>'
+            currentPlayer = get_player_stats(i)
+            playerSpecs = '<tr><td><a href="https://www.nhl.com/player/'+str(i["person"]["id"])+'"target="_blank">'+currentPlayer[0]+'</a></td>'
+            list_iter = iter(currentPlayer)
+            next(list_iter)
             currentTeam = currentTeam + playerSpecs
             #Goalie stats
             if (i["position"]["abbreviation"]=='G'):
-              win = request.args.get('win', type = float) * playerStats["wins"]
-              ga = request.args.get('ga', type = float) * playerStats["goalsAgainst"]
-              save = request.args.get('save', type = float) * playerStats["saves"]
-              shutout = request.args.get('so', type = float) * playerStats["shutouts"]
-              fantasyScore = round(win + ga + save + shutout, 1)
-              currentTeam = currentTeam + '<td>' + str(fantasyScore) + '</td></tr>'
-              currentGoalies = currentGoalies+'<tr>'+playerSpecs + '</td><td><a href="'+x["officialSiteUrl"]+'" target="_blank">'+x["name"]+'</a></td><td>'+ str(fantasyScore)+'</td><td>'+ str(playerStats["wins"])+'</td><td>'+ str(playerStats["goalsAgainst"])+'</td><td>'+ str(playerStats["saves"])+'</td><td>'+ str(playerStats["shutouts"])+'</tr>'
-            #Skater stats
+              currentTeam = currentTeam + '<td>'+currentPlayer[1] + '</td><td>'+ currentPlayer[2] + '</td><td>'+ currentPlayer[3] + '</td></tr>'
+              for j in list_iter:
+                currentGoalies = currentGoalies +'<td>'+j +'</td>'
+              #Skater stats
             else:
-              goals = request.args.get('goals', type = float) * playerStats["goals"]
-              assists = request.args.get('assists', type = float) * playerStats["assists"]
-              pim = request.args.get('pim', type = float) * playerStats["pim"]
-              ppp = request.args.get('ppp', type = float) * playerStats["powerPlayPoints"]
-              shp = request.args.get('shp', type = float) * playerStats["shortHandedPoints"]
-              sog = request.args.get('sog', type = float) * playerStats["shots"]
-              hit = request.args.get('hit', type = float) * playerStats["hits"]
-              blk = request.args.get('blk', type = float) * playerStats["blocked"]
-              fantasyScore = goals + assists + pim + ppp + shp + sog + hit + blk
-              currentTeam = currentTeam + '<td>' + str(fantasyScore)+'</td><td>'+ str(playerStats["games"])+'</td><td>'+str(playerStats["goals"])+'</td><td>'+str(playerStats["assists"])+'</td><td>'+str(playerStats["points"])+'</td><td>'+str(playerStats["pim"])+'</td><td>'+str(playerStats["powerPlayPoints"])+'</td><td>'+str(playerStats["shortHandedPoints"])+'</td><td>'+str(playerStats["shots"])+'</td><td>'+str(playerStats["hits"])+'</td><td>'+str(playerStats["blocked"])+'</td></tr>'
+              for j in list_iter:
+                currentTeam = currentTeam +'<td>'+ j +'</td>'
+            currentTeam = currentTeam + '</tr>'
           #This only happens for players who are on the roster, but haven't played any games
           except IndexError:
             print("No stats for "+i["person"]["fullName"])
@@ -84,7 +174,8 @@ def index():
 
 @app.route('/my-link/')
 def my_link():
-  return return_teams()
+  #return return_teams()
+  return return_players_last_season()
 
 
 if __name__ == '__main__':
